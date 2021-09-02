0cb266560153ed2c61a1fcebc19351373a6d2290
Hailson Junior <79525895+HailsonJunior@users.noreply.github.com>
2021-09-02 13:45:51 -0300

    teste drone ci

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,17 +1,5 @@
-data "aws_ami" "ubuntu" {
-    name = "ubuntu"
-    most_recent = true
-
- #   filter {
- #       name = "name"
- #       values = ["IaaSWeek-${var.hash_commit}"] 
- #   } 
-
-    owners = [ "986332348173" ]
-}
-
 resource "aws_instance" "web" {
-    ami = "data.aws_ami.ubuntu.id"
+    ami = "ami-09e67e426f25ce0d7"
     instance_type = "t2.micro"
     subnet_id = "subnet-3a5ad31b"
     vpc_security_group_ids = [ "${aws_security_group.allow_http.id}" ]
